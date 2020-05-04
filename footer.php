b30a97b8995f760b0f06fad8c9d79636b73b5e73
sohampatelk <sohampatelk@yahoo.com>
2020-05-04 16:18:28 -0600

    example-theme

--- footer.php
+++ footer.php
@@ -5,7 +5,13 @@
         <?php echo date('Y'); //use PHP to keep the year current.?>
         TechCarrers by Manpower
     </p>
+    <?php if (function_exists('output_hello_world')) { ?>
+        <aside>
+            <?php output_hello_world(); ?>
+        </aside>
+    <?php } ?>
 </footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
